243689cc9ddedc331fbab1e383db35b55ecc759a
Mikadore <anonymous1337mm@gmail.com>
2024-02-22 08:14:06 +0100

    Barebones CLI structure

--- pygenpp/cli.py
+++ pygenpp/cli.py
@@ -1,5 +1,26 @@
+import click, sh, os, sys
+import logging4
+from pathlib import Path
+
+
+logger = logging4.Logger(name="MyLogger")
+formatter = '[[time]] - [[name]] - [[level_name]] - [[msg]]'
+logger.add_channel(filename=sys.stdout, level=logging4.DEBUG, formatter=formatter)
+
+
+@click.group()
 def cli():
-    print("hello world")
+    pass
+
+@click.command()
+def init():
+    dirpath = os.getcwd()
+    assert len(os.listdir(dirpath)) == 0, "Directory must be empty"
+    logger.info(f"Initializing directory {dirpath}")
+    
+        
+
+cli.add_command(init)
 
 if __name__ == "__main__":
     cli()
